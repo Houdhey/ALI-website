7725eb6df95970c590c1c3b7df1ce954e1e85aef
Houdhey <houdheyfa.bouzid@gmail.com>
2022-08-26 21:27:33 +0200

    Ca prend forme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestpageComponent } from './testpage/testpage.component';
+import player from 'lottie-web';
+import { LottieModule } from 'ngx-lottie';
 
+export function playerFactory() {
+  return player;
+}
 @NgModule({
   declarations: [AppComponent, TestpageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LottieModule.forRoot({ player: playerFactory }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/testpage/testpage.component.ts
+++ src/app/testpage/testpage.component.ts
@@ -17,17 +17,49 @@ export class TestpageComponent implements OnInit {
   onResize(event: { target: { innerWidth: any } }) {
     this.innerWidth = event.target.innerWidth;
     console.log('width actuel : ' + this.innerWidth);
+    this.resizeImage();
   }
   resizeImage() {
     const myId = document.getElementById('myFlexContainer');
+    const flexContainer1 = document.getElementById('flexContainer1');
+
+    const customContainerRight = document.getElementById(
+      'customContainerRight'
+    );
+    const carImage = document.getElementById('carImage');
+    const carImage2 = document.getElementById('carImage2');
+    const carImage3 = document.getElementById('carImage3');
     if (this.innerWidth < 993) {
       console.log('resizing image');
 
-      // @ts-ignore
-      myId.style.flexDirection = 'column';
+      myId!.style.flexDirection = 'column';
+
+      flexContainer1!.style.marginLeft = '0px';
+
+      customContainerRight!.style.visibility = 'none';
+      customContainerRight!.style.marginLeft = '0px';
+
+      carImage!.style.width = '100%';
+      carImage!.style.height = '100%';
+      carImage!.style.marginLeft = '0px';
+
+      carImage3!.style.width = '100%';
+      carImage3!.style.height = '100%';
+      carImage3!.style.marginLeft = '0px';
     } else {
-      // @ts-ignore
-      myId.style.flexDirection = 'row';
+      myId!.style.flexDirection = 'row';
+
+      flexContainer1!.style.marginLeft = '80px';
+
+      customContainerRight!.style.visibility = 'visible';
+
+      carImage!.style.width = '1100px';
+      carImage!.style.height = '1100px';
+      carImage!.style.marginLeft = '-300px';
+
+      carImage3!.style.width = '1100px';
+      carImage3!.style.height = '1100px';
+      carImage3!.style.marginLeft = '-300px';
     }
   }
 }
