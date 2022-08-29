0316fcff167980b8ae27244b1e89cb81fdc9a45e
Houdhey <houdheyfa.bouzid@gmail.com>
2022-08-29 15:25:48 +0200

    now is responsive

--- src/app/testpage/testpage.component.ts
+++ src/app/testpage/testpage.component.ts
@@ -46,6 +46,13 @@ export class TestpageComponent implements OnInit {
       carImage3!.style.width = '100%';
       carImage3!.style.height = '100%';
       carImage3!.style.marginLeft = '0px';
+
+      document.getElementById('bigTitle').style.fontSize = '60px';
+
+      document.getElementById('section3part1').style.flexDirection = 'column';
+      document.getElementById('section3part2').style.flexDirection = 'column';
+      document.getElementById('section3part2Div1').style.marginLeft = '50px';
+      document.getElementById('section3part3').style.flexDirection = 'column';
     } else {
       myId!.style.flexDirection = 'row';
 
@@ -60,6 +67,13 @@ export class TestpageComponent implements OnInit {
       carImage3!.style.width = '1100px';
       carImage3!.style.height = '1100px';
       carImage3!.style.marginLeft = '-300px';
+
+      document.getElementById('bigTitle').style.fontSize = '80px';
+
+      document.getElementById('section3part1').style.flexDirection = 'row';
+      document.getElementById('section3part2').style.flexDirection = 'row';
+      document.getElementById('section3part2Div1').style.marginLeft = '200px';
+      document.getElementById('section3part3').style.flexDirection = 'row';
     }
   }
 }
